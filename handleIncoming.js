8a47cecbafa36242bfe2bbfdac91dbae28750f31
Shantanu Srivastava <shantanu.srivastava@neuraflash.com>
2019-02-05 23:33:57 +0530

    Debugging

--- handleIncoming.js
+++ handleIncoming.js
@@ -1,6 +1,8 @@
 var handleIncoming = {};
 
 handleIncoming.getTime = function(requestBody){
+    console.log('>> text : ' + requestBody.message.text);
+    console.log('>> return response : ' + JSON.stringify('text', 'You said : ' + requestBody.message.text));
     return  JSON.stringify('text', 'You said : ' + requestBody.message.text);
 }    
 
