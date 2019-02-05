024c17438f6496c6edf066a1f12e072094487daf
Shantanu Srivastava <shantanu.srivastava@neuraflash.com>
2019-02-05 22:46:42 +0530

    Handle Basic Post Request

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ var request = require('request');
 var bodyParser = require("body-parser");
 var express = require('express');
 var app = express();
+var http = require('http');
+var http = require('http').Server(app);
 
 app.set('port', process.env.PORT || 8080);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post("/", function (req, res) {
-    console.log('>> request body : ' + req.body);
+    console.log('>> request body : ' + JSON.stringify(req.body));
 });
-app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+
+http.listen(process.env.PORT || 8080, function () {
+    console.log("Listening on port : " + process.env.PORT);
 });
